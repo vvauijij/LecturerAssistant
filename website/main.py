fa3a8719f02f8e2135807297eaae1708b8e73223
Konstantin Sechkar <sechkosta2004@gmail.com>
2022-12-07 20:36:17 +0300

    забыл в прикрутке бота

--- website/main.py
+++ website/main.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
-from app import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+    return redirect(url_for("auth.login"))
 
 @main.route('/profile')
 @login_required
