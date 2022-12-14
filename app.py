adec564b12b65261a2c50bfc14cbe6f4b5362b92
Алексей Пеньков <vvauijij@MacBook-Air-Aleksej.local>
2022-12-14 11:30:19 +0300

    fixed circular import

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from flask_migrate import Migrate
 import os
 import threading
 
-
-from models import User
-from auth import auth as auth_blueprint
-from main import main as main_blueprint
-from user_in import user_in as user_in_blueprint
-
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -29,15 +23,19 @@ login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
 
+from models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
 
 
+from main import main as main_blueprint
 app.register_blueprint(main_blueprint)
 
 
+from user_in import user_in as user_in_blueprint
 app.register_blueprint(user_in_blueprint)
